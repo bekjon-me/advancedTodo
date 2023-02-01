d5f9e2d370bf7afa0f9c0893d5df2ac66a186cbf
Jahongir Qurbonov <qurbonovjahongir17@gmail.com>
2023-02-02 00:03:51 +0500

    add tests

--- server/projects/tests.py
+++ server/projects/tests.py
@@ -1,31 +1,66 @@
-# from django.test import TestCase
-# from django.contrib.auth.models import User
-# from django.contrib.admin.sites import AdminSite
-# from django.contrib.admin.options import ModelAdmin
-# from .admin import ProjectAdmin
-# from .models import Project
-# Create your tests here.
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
+from .models import Project
 
 
-# class ProjectAdminTests(TestCase):
+class TaskTests(APITestCase):
+    def setUp(self):
+        super(TaskTests, self).setUp()
+        self.username = "username"
+        self.password = "Pas$w0rd"
+        self.data = {
+            "username": self.username,
+            "password": self.password,
+        }
+        self.user_model = get_user_model()
 
-#     @classmethod
-#     def setUpTestData(cls):
-#         cls.user = User.objects.create(
-#             username='jahongir',
-#             is_superuser=True,
-#         )
-#         cls.project = Project.objects.create(
-#             user=cls.user,
-#             name='NewProject'
-#         )
+        url = reverse("rest_login")
+        user = self.user_model.objects.create_user(
+            username=self.username, password=self.password
+        )
+        response = self.client.post(url, self.data, format="json")
+        access_token = response.data["access_token"]
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer {0}".format(access_token))
 
-#     def sutUp(self):
-#         self.site = AdminSite()
+    def test_create_project(self):
+        name_ = "P1"
 
-#     def test_modeladmin_str(self):
-#         ma = ModelAdmin(Project, self.site)
-#         self.assertEqual(str(ma),
-#             f"User:{self.user} ID:{self.upid} Name:{self.name} "
-#             f"(Date:{self.created.strftime('%m/%d/%Y|%H:%M:%S')})"
-#         )
+        url = reverse("projects-list")
+        resp = self.client.post(url, {"name": name_})
+        data = resp.json()
+
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(data["upid"], 1)
+        self.assertEqual(data["name"], name_)
+        self.assertTrue(data["created"])
+        self.assertTrue(data["updated"])
+
+    def test_get_projects(self):
+        n1, n2, n3 = "P1", "P2", "P3"
+        user_ = self.user_model.objects.get(pk=1)
+        for name in [n1, n2, n3]:
+            Project.objects.create(name=name, user=user_)
+
+        url = reverse("projects-list")
+        resp = self.client.get(url)
+        data = resp.json()
+
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(data.__len__(), 3)
+
+    def test_get_project(self):
+        name_ = "P1"
+        Project.objects.create(name=name_, user=self.user_model.objects.get(pk=1))
+
+        url = reverse("project-detail", kwargs={"upid": 1})
+        resp = self.client.get(url)
+        data = resp.json()
+
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(data["upid"], 1)
+        self.assertEqual(data["name"], name_)
+        self.assertTrue(data["created"])
+        self.assertTrue(data["updated"])
+
--- server/tasks/tests.py
+++ server/tasks/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from myproject.apps.core.models import Account
 
-# Create your tests here.
+
+class TaskTests(APITestCase):
+    def setUp(self):
+        self.url = reverse('tasks-list')
+
+    def test_create_account(self):
+        url = reverse('tasks-list')
+        data = {'name': 'DabApps'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Account.objects.count(), 1)
+        self.assertEqual(Account.objects.get().name, 'DabApps')
